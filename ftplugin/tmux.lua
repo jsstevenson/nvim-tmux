daee81cb4b86105a3f8cfc29805a136ebc5ef4de
James Stevenson <james.stevenson@nationwidechildrens.org>
2022-08-14 09:47:35 -0400

    fix: add folders to runtime

--- ftplugin/tmux.lua
+++ ftplugin/tmux.lua
@@ -2,6 +2,7 @@ local group = vim.api.nvim_create_augroup("TmuxFilePlugin", { clear = true })
 vim.api.nvim_create_autocmd({ "BufNew", "BufEnter", "BufNewFile" }, {
   callback = function()
     vim.bo.compiler = "tmux"
+    vim.api.nvim_echo("in tmux")
   end,
   group = group,
 })
