0a7442954a44803318a700f41053cb381d0f0367
James Stevenson <james.stevenson@nationwidechildrens.org>
2022-08-14 11:07:21 -0400

    feat: set compiler for tmux filetype

--- ftplugin/tmux.lua
+++ ftplugin/tmux.lua
@@ -1,8 +1,7 @@
 local group = vim.api.nvim_create_augroup("TmuxFilePlugin", { clear = true })
 vim.api.nvim_create_autocmd({ "BufNew", "BufEnter", "BufNewFile" }, {
   callback = function()
-    vim.bo.compiler = "tmux"
-    vim.api.nvim_echo("in tmux")
+    vim.cmd("compiler tmux")
   end,
   group = group,
 })
