a7d38e4e29b1fdf1a456997f0a52671dff9e1a48
James Stevenson <james.stevenson@nationwidechildrens.org>
2022-08-13 21:05:57 -0400

    feat: run selected commands

--- lua/tmux_utils.lua
+++ lua/tmux_utils.lua
@@ -16,6 +16,7 @@ end
 -- don't remove formatting
 -- how to provide user-configs, default configs
 -- have separate lsp-hover-like option
+-- add term -> manpage term translator table
 local function make_floatwin(term)
   local config_defaults = {
     man_height = 0.85,
@@ -65,23 +66,88 @@ function M.show_man_floatwin()
   make_floatwin(search_term)
 end
 
+function make_chunk_arrays(result, hl_group)
+  local r = {}
+  for i, v in ipairs(result) do
+    r[i] = { v .. "\n", hl_group }
+  end
+  return r
+end
+
 -- execute line as tmux command
-function exec_tmux_cmd(line)
+-- TODO
+-- capture and print error output
+local function exec_tmux_cmd(line)
   local Job = require("plenary.job")
+
+  local result
+  local return_value
 
   Job:new({
     command = "tmux",
     args = line,
     on_exit = function(j, return_val)
-      if return_val == 1 then
-        print("Command failed: " .. "tmux" .. table.concat(line, " "))
-      end
+      result = j:result()
+      return_value = return_val
     end,
   }):sync()
+
+  local arrayed_chunks
+  if return_value == 0 then
+    arrayed_chunks = make_chunk_arrays(result)
+  else
+    arrayed_chunks = make_chunk_arrays({ "command failed: " .. table.concat(line, " ") }, "Error")
+  end
+  vim.api.nvim_echo(arrayed_chunks, false, {})
+  return return_value
+end
+
+-- This is copied ~verbatim from... somewhere on Reddit or Stackoverflow.
+local function get_visual_selection()
+  local s_start = vim.fn.getpos("'<")
+  local s_end = vim.fn.getpos("'>")
+  local n_lines = math.abs(s_end[2] - s_start[2]) + 1
+  local lines = vim.api.nvim_buf_get_lines(0, s_start[2] - 1, s_end[2], false)
+  lines[1] = string.sub(lines[1], s_start[3], -1)
+  if n_lines == 1 then
+    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3] - s_start[3] + 1)
+  else
+    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3])
+  end
+  return table
+end
+
+-- TODO
+-- more thorough testing, incl. with keymaps
+function M.tmux_exec_current_lines()
+  local position = vim.fn.getpos(".")
+  local visual_position = vim.fn.getpos("v")
+  if position[2] ~= visual_position[2] then
+    local lines = get_visual_selection()
+    for _, line in ipairs(lines) do
+      local return_value = exec_tmux_cmd(line)
+      if return_value ~= 0 then
+        break
+      end
+    end
+  else
+    local line = vim.api.nvim_buf_get_lines(0, position[2] - 1, position[2], false)
+    exec_tmux_cmd(line)
+  end
 end
 
 -- keyword/highlight group based jump (????)
 
 -- testing
+
+
+
+local test = [[
+resize-pane -R 1
+resize-pane -L 2
+list-clients
+list-commands
+reskize-pane -R 1
+]]
 
 return M
